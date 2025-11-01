5c5be0459dfcaa00d22b5df6f7ade04cf518d9d0
heath-jhs <heath@jhstrickland.com>
2025-11-01 09:49:22 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1d4ed8', // blue-700
+        secondary: '#6b7280' // gray-500
+      }
+    },
+  },
+  plugins: [],
+}
