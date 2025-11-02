8da2fdc11c191f2b576a3e7fd176b2c0e4ad55af
heath-jhs <heath@jhstrickland.com>
2025-11-02 09:31:27 -0500

    Update EmployeeHistory.jsx

--- src/components/EmployeeHistory.jsx
+++ src/components/EmployeeHistory.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { supabase } from '../supabase';
+import { supabase } from '../supabaseClient.js';
 import { format } from 'date-fns';
 
 export default function EmployeeHistory({ user }) {
