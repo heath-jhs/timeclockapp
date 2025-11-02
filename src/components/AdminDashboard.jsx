742c1358aeef2ae534e5b5413b90e06b1c7985a3
heath-jhs <heath@jhstrickland.com>
2025-11-02 09:30:53 -0500

    Update AdminDashboard.jsx

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { supabase } from '../supabase';
+import { supabase } from '../supabaseClient.js';
 import EmployeeScheduleModal from './EmployeeScheduleModal';
 
 export default function AdminDashboard({ user }) {
