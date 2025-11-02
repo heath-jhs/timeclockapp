98f48d2560db500fdff4b17d9dd2a0246fcc0864
heath-jhs <heath@jhstrickland.com>
2025-11-02 09:33:33 -0500

    Update EmployeeScheduleModal.jsx

--- src/components/EmployeeScheduleModal.jsx
+++ src/components/EmployeeScheduleModal.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { supabase } from '../supabase';
+import { supabase } from '../supabaseClient.js';
 
 const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
