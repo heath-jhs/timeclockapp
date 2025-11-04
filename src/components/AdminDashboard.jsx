d5ab28b685af8a42f0f26798bbcb02e75f620d27
heath-jhs <heath@jhstrickland.com>
2025-11-04 09:25:30 -0500

    Update AdminDashboard.jsx

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -3,12 +3,15 @@ import { createClient } from '@supabase/supabase-js';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
 });
 
 const AdminDashboard = ({ logout }) => {
