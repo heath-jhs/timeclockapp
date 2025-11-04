14009792e4c0a10b249c073d7d1363bbb8437aac
heath-jhs <heath@jhstrickland.com>
2025-11-03 19:25:53 -0500

    Update delete-user.js

--- netlify/functions/delete-user.js
+++ netlify/functions/delete-user.js
@@ -16,7 +16,7 @@ exports.handler = async (event) => {
         detectSessionInUrl: false
       }
     });
-    const { error } = await supabase.auth.admin.deleteUser(userId, { shouldSoftDelete: true });
+    const { error } = await supabase.auth.admin.deleteUser(userId, { should_soft_delete: true });
     if (error) {
       console.log('Delete user error details:', error);
       throw error;
