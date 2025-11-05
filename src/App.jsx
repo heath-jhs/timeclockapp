e95ec239f539b1c7e8cd6af95e99cbbe7c51096b
heath-jhs <heath@jhstrickland.com>
2025-11-05 09:04:23 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,6 @@ export default function App() {
     <SessionContextProvider supabaseClient={supabase}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/invite" element={<InviteSetup />} />
 
